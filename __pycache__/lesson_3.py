4bf632d8a56f09f058aba0e87e8a90ef742e5eaa
asabale221 <119834965+asabale221@users.noreply.github.com>
2022-12-13 20:33:14 +0600

    sozdal fayl

--- __pycache__/lesson_3.py
+++ __pycache__/lesson_3.py
@@ -1,31 +1,42 @@
-#инкапсуляция
-#git clone
+# #инкапсуляция
+# #git clone
 
 
-# инкапсуляция заключение в одно место (класс) для работы с ними 2 предаставление пользователя публичного интерсейфа
+# # инкапсуляция заключение в одно место (класс) для работы с ними 2 предаставление пользователя публичного интерсейфа
 
 
 
 
-#public без нижны почедчеркований
-#публичный
-#защищеный
-# скрытый
+# #public без нижны почедчеркований
+# #публичный
+# #защищеный
+# # скрытый
 
 
-class Human:
-    atribyt=1
-    def __init__(self,name,age):
-        self.__name=name
-        self.age=age
-    def run(self):
-        print(f'{self.__name} is running')
+# class Human:
+#     atribyt=1
+#     def __init__(self,name,age):
+#         self.__name=name
+#         self.age=age
+#     def run(self):
+#         print(f'{self.__name} is running')
 
-h=Human('Азиреть', 20)
-# h.name='gyuy'
-# h._name='Daniel'
-# print(h.name,h._name)
-# print(_Human_.__name)
+#     def prin(self):
+#         return self.__name
+#     def set_prin(self,name):
+#         self.__name=name
+    
+# h=Human('Азиреть', 20)
+# # h.name='gyuy'
+# # h._Human__run()
+# # print(h.name,h._name)
+# # print(_Human_.__name)
+# print(h.set_prin('Beka'))
+# print(h.get_prin())
+
+
+
+
 
 
 
